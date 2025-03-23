5afd8dfdc1f1914e2c5fd77195698878a9ac8308
Tobias Prandini <tobiasprandini@gmail.com>
2025-03-23 02:49:35 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Hello world</h1>
-    </div>
+    <Button>Hello World</Button>
   );
 };
 
