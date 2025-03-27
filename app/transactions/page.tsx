f9b2cee099e765c8c1034705e139bf8169c5d815
Tobias Prandini <tobiasprandini@gmail.com>
2025-03-26 22:00:18 -0300

    feat: ensure transaction can be updated

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,18 +3,17 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 
-
 const TransactionsPage = async () => {
   // Acessar as transações do meu banco de dados
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="p-6 space-y-6">
+    <div className="space-y-6 p-6">
       {/* TÍTULO E BOTÃO */}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
         <AddTransactionButton />
       </div>
-      <DataTable columns={transactionColumns} data={transactions}/>
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 };
