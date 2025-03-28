604a3f9124a48dde746c21144562c684e4678a76
Tobias Prandini <tobiasprandini@gmail.com>
2025-03-28 16:09:31 -0300

    feat: add summary cards

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { Transaction } from "@prisma/client";
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import TransactionTypeBadge from "../_components/type-badge";
-import { Button } from "@/app/_components/ui/button";
-import { TrashIcon } from "lucide-react";
-import {
-  TRANSACTION_CATEGORY_LABELS,
-  TRANSACTION_PAYMENT_METHOD_LABELS,
-} from "@/app/_constants/transactions";
-import EditTransactionButton from "../_components/edit-transaction-button";
+import { CircleIcon } from "lucide-react";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,53 +13,44 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "type",
     header: "Tipo",
-    cell: ({ row: { original: transaction } }) => (
-      <TransactionTypeBadge transaction={transaction} />
-    ),
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return (
+          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            Depósito
+          </Badge>
+        );
+      }
+      if (transaction.type === TransactionType.EXPENSE) {
+        return (
+          <Badge className="font-bold text-danger bg-danger bg-opacity-10">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            Despesa
+          </Badge>
+        );
+      }
+      return "Investimento";
+    },
   },
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({ row: { original: transaction } }) =>
-      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
-    cell: ({ row: { original: transaction } }) =>
-      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
-    cell: ({ row: { original: transaction } }) =>
-      new Date(transaction.date).toLocaleString("pt-BR", {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
-    cell: ({ row: { original: transaction } }) =>
-      new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
-    header: "Ações",
-    cell: ({ row: { original: transaction } }) => {
-      return (
-        <div className="space-x-1">
-          <EditTransactionButton transaction={transaction} />
-          <Button variant="ghost" size="icon" className="text-muted-foreground">
-            <TrashIcon />
-          </Button>
-        </div>
-      );
-    },
+    header: "",
   },
 ];
